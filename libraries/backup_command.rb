92cc2c4845337eb0b8328a5194c5465e1a840661
Andy Fleener <andrew.fleener@sportngin.com>
2015-01-02 14:43:30 -0600

    Add before, after, and failure commands for backup cron

---
+++ libraries/backup_command.rb
@@ -0,0 +1,44 @@
+module EbsBackups
+  class BackupCommand
+    def self.command(user, params)
+      new(user, params).command
+    end
+
+    def initialize(user, params)
+      @params = params
+      @user = user
+    end
+
+    def command
+      [before_command, "/home/#{@user}/automated-backup.sh#{backup_options}", after_command, failure_command, log].compact.join(" ")
+    end
+
+    def before_command
+      "#{@params[:before]} &&" if @params[:before]
+    end
+
+    def after_command
+      "&& #{@params[:after]}" if @params[:after]
+    end
+
+    def failure_command
+      "|| #{@params[:failure]}" if @params[:failure]
+    end
+
+    def log
+      "&> /home/#{@user}/automated-backup_`date +\"%Y%m%d\"`.log" if @params[:log]
+    end
+
+    def backup_options
+      options = ""
+      options << " -s #{@params[:selection_method]}" if @params[:selection_method]
+      options << " -t '#{@params[:tag]},Values=true'" if @params[:tag]
+      options << " -v '#{@params[:volumeid]}'" if @params[:volumeid]
+      options << " -r #{@params[:region]}" if @params[:region]
+      options << " -c #{@params[:cron_primer_file]}" if @params[:cron_primer_file]
+      options << " -k #{@params[:purge_after_days]}" if @params[:purge_after_days]
+      options << " -p" if @params[:purge]
+      options
+    end
+  end
+end
--- test/integration/default/serverspec/aws_backup_spec.rb
+++ test/integration/default/serverspec/aws_backup_spec.rb
@@ -4,7 +4,10 @@ describe file('/home/deploy/automated-backup.sh') do
   it { should be_file }
 end
 
-# describe cron do
-#   it { should have_entry("1 * * * 6 /home/deploy/ec2-automate-backup-awscli.sh -t 'Backups=true' -k 30") }
-# end
-# Serverspec cron matcher is not working at the moment
+describe cron do
+  it { should have_entry("1 * * * 6 /home/ec2-user/automated-backup.sh -t 'Backups,Values=true' -k 30").with_user('ec2-user') }
+  it { should have_entry("2 * * * 6 land before time && /home/ec2-user/automated-backup.sh -t 'Before,Values=true' -k 30").with_user('ec2-user') }
+  it { should have_entry("3 * * * 6 /home/ec2-user/automated-backup.sh -t 'After,Values=true' -k 30 && happily ever after").with_user('ec2-user') }
+  it { should have_entry("4 * * * 6 /home/ec2-user/automated-backup.sh -t 'Failure,Values=true' -k 30 || epic fail").with_user('ec2-user') }
+  it { should have_entry("5 * * * 6 land before time && /home/ec2-user/automated-backup.sh -t 'All,Values=true' -k 30 && happily ever after || epic fail").with_user('ec2-user') }
+end
